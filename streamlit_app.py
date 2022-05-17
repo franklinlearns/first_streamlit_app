e3e2c596912abeddb48d35a9f7fe8a1dc02889a1
Franklin Fallas <105607635+franklinlearns@users.noreply.github.com>
2022-05-17 06:11:07 -0600

    Update streamlit_app.py
    input added to allow inserting fruits into FRUITS_LIST

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,9 +35,15 @@ streamlit.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
+
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The Fruit Load List Contains:")
 streamlit.dataframe(my_data_rows)
+
+streamlit.text()
+add_my_fruit = streamlit.text_input("What fruit would you like to add?")
+streamlit.write('The user entered', add_my_fruit)
+my_cur.execute("INSERT into fruit_load_list(FRUIT_NAME) values (" + add_my_fruit + ")")
 
 
