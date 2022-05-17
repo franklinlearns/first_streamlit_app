60ba86f940afb09656a0b5e189886403640abd31
Franklin Fallas <105607635+franklinlearns@users.noreply.github.com>
2022-05-17 06:13:23 -0600

    Update streamlit_app.py
    default value added

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,8 +41,8 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The Fruit Load List Contains:")
 streamlit.dataframe(my_data_rows)
 
-add_my_fruit = streamlit.text_input("What fruit would you like to add?")
+add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
 streamlit.write('The user entered', add_my_fruit)
-my_cur.execute("INSERT into fruit_load_list(FRUIT_NAME) values (" + add_my_fruit + ")")
+my_cur.execute('INSERT into fruit_load_list(FRUIT_NAME) values (' + add_my_fruit + ')')
 
 
